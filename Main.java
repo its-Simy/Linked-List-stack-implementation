bf1bccdbccb15380efd1b477923b351226e74478
its-Simy <135281067+its-Simy@users.noreply.github.com>
2024-10-24 10:34:41 -0400

    functionality demonstration implementation
    Added the working parts to the main that shows the working parts of the LinkedListStack class.

--- Main.java
+++ Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
+LinkedListStack lstack = new LinkedListStack();
+
+lstack.push(5);
+System.out.println(lstack.peek());//makes first element 5
+
+lstack.push(10);
+System.out.println(lstack.peek());//makes the first element 5
+
+lstack.pop();
+System.out.println(lstack.peek());//removes the first element, makes second element the first one
 
 
 
